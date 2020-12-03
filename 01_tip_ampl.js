eb240140e65a63d1b435535886bd8b9e2263f1e5
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2020-12-03 13:26:12 +0200

    Tipping Ampl
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- 01_tip_ampl.js
+++ 01_tip_ampl.js
@@ -6,107 +6,109 @@
 //truffle exec 01_tip_ampl.js --network mainnet
 
 require('dotenv').config()
-const Web3 = require('web3')
-const HDWalletProvider = require("@truffle/hdwallet-provider")
-const fs = require('fs')
+const ethers = require('ethers');
 const fetch = require('node-fetch-polyfill')
+const path = require("path")
+const loadJsonFile = require('load-json-file');
 
-const TellorMaster = artifacts.require("./TellorMaster.sol")
-const Oracle = artifacts.require("./Tellor.sol")
-const oracleAbi = Oracle.abi
-const oracleByte = Oracle.bytecode
-const abiAMPLinter = artifacts.require("./abi/abiAmplIntermediate")
-
+const pubAddr = process.env.ETH_PUB;
+const privKey = process.env.ETH_PK;
+const infuraKey = process.env.INFURA_TOKEN;
 
 function sleep_s(secs) {
-  secs = (+new Date) + secs * 1000;
-  while ((+new Date) < secs);
+    secs = (+new Date) + secs * 1000;
+    while ((+new Date) < secs);
 }
 
 //https://ethgasstation.info/json/ethgasAPI.json
 //https://www.etherchain.org/api/gasPriceOracle
 async function fetchGasPrice() {
-  const URL = `https://www.etherchain.org/api/gasPriceOracle`;
-  try {
-    const fetchResult = fetch(URL);
-    const response = await fetchResult;
-    const jsonData = await response.json();
-    const gasPriceNow = await jsonData.fast*1;
-    const gasPriceNow2 = await (gasPriceNow)*1000000000;
-    console.log(jsonData);
-    return(gasPriceNow2);
-  } catch(e){
-    throw Error(e);
-  }
+    const URL = `https://www.etherchain.org/api/gasPriceOracle`;
+    try {
+        const fetchResult = fetch(URL);
+        const response = await fetchResult;
+        const jsonData = await response.json();
+        const gasPriceNow = await jsonData.fast * 1;
+        const gasPriceNow2 = await (gasPriceNow) * 1000000000;
+        return (gasPriceNow2);
+    } catch (e) {
+        throw Error(e);
+    }
 }
 
-var pk = process.env.ETH_PK
-var accessToken = process.env.INFURA_ACCESS_TOKEN
-//var web3 = new Web3(new HDWalletProvider(pk,"https://rinkeby.infura.io/v3"+ accessToken))
-
-var web3 = new Web3(new HDWalletProvider(pk,"https://mainnet.infura.io/v3/" + accessToken))
-var _UTCtime  = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-var gas_Limit= 400000
+var _UTCtime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+var gas_limit = 400000
 
 //Rinkeby
-//var tellorMasterAddress = '0xFe41Cb708CD98C5B20423433309E55b53F79134a' 
-//var accountFrom = '0xe010ac6e0248790e08f42d5f697160dedf97e024'
+const tellorMasterAddress = '0xFe41Cb708CD98C5B20423433309E55b53F79134a'
+const network = "rinkeby";
 
-var tellorMasterAddress = '0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5'
-var accountFrom = '0xf967b011c0b04913d16bf8807f38d7346112a56b'
+// var tellorMasterAddress = '0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5'
+// const network = "mainnet";
+
 
 console.log(_UTCtime)
 console.log("Tellor Address: ", tellorMasterAddress)
-console.log('<https://www.etherchain.org/api/gasPriceOracle>')
+console.log('https://www.etherchain.org/api/gasPriceOracle')
 
-module.exports =async function(callback) {
-    try{
+let run = async function () {
+    try {
         var gasP = await fetchGasPrice()
         console.log("gasP1", gasP)
-    } catch(error){
+    } catch (error) {
         console.error(error)
         console.log("no gas price fetched")
+        process.exit(1)
     }
 
-    let tellorMaster = await TellorMaster.at(tellorMasterAddress)
-    try{
-        var balNow = await web3.eth.getBalance(accountFrom)
-        console.log("Requests Address", accountFrom)
-        var currBalance = await web3.utils.fromWei(balNow,'ether')
-        console.log("Requester ETH Balance",  currBalance)
-        var ttbalanceNow = await tellorMaster.balanceOf(accountFrom)
-        var tributesBal =  await web3.utils.fromWei(ttbalanceNow,'ether')
-        console.log('Tellor Tributes balance', tributesBal)
-        var txestimate = gasP * gas_limit
-    } catch(error) {
+    try {
+        var provider = ethers.getDefaultProvider(network, infuraKey);
+        let wallet = new ethers.Wallet(privKey, provider);
+        let abi = await loadJsonFile(path.join("abi", "tellor.json"))
+        let contract = new ethers.Contract(tellorMasterAddress, abi, provider);
+        var contractWithSigner = contract.connect(wallet);
+
+    } catch (error) {
         console.error(error)
+        console.log("oracle not instantiated")
+        process.exit(1)
     }
-    
-    if (gasP != 0 && txestimate > balNow && ttbalanceNow > 1 ) {
-        try{
-            var oracle = await new web3.eth.Contract(oracleAbi,tellorMasterAddress);
-            console.log("awaitOracle")
-        } catch(error) {
-            console.error(error)
-            console.log("oracle not instantiated")
-        }
 
+
+    try {
+        var balNow = ethers.utils.formatEther(await provider.getBalance(pubAddr))
+        console.log("Requests Address", pubAddr)
+        console.log("Requester ETH Balance", balNow)
+        var ttbalanceNow = ethers.utils.formatEther(await contractWithSigner.balanceOf(pubAddr))
+        console.log('Tellor Tributes balance', ttbalanceNow)
+        var txestimate = (gasP * gas_limit / 1e18);
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+
+    if (gasP != 0 && txestimate < balNow && ttbalanceNow > 1) {
         console.log("Send request for AMPL")
-        try{
-            await oracle.methods.addTip(10,1).send({from: accountFrom,gas: gas_Limit,gasPrice: gasP })
-            .on('transactionHash', function(hash){
-                var link = "".concat('<https://etherscan.io/tx/',hash,'>' )
-                var ownerlink = "".concat('<https://etherscan.io/address/',tellorMasterAddress,'>' )
-                 console.log('Yes, a request was sent for the APML price')
-                console.log("Hash link: ", link)
-                console.log("Contract link: ", ownerlink)
-            })
-            .on('error', console.error) // If there's an out of gas error the second parameter is the receipt.
-        } catch(error) {
-        console.error(error)
+        try {
+            var gasP = await fetchGasPrice()
+
+            let tx = await contractWithSigner.addTip(10, 1, { from: pubAddr, gasLimit: gas_limit, gasPrice: gasP });
+            var link = "".concat('https://etherscan.io/tx/', tx.hash)
+            var ownerlink = "".concat('https://etherscan.io/address/', tellorMasterAddress)
+            console.log('Yes, a request was sent for the APML price')
+            console.log("Hash link: ", link)
+            console.log("Contract link: ", ownerlink)
+            console.log('Waiting for the transaction to be mined');
+            await tx.wait() // If there's an out of gas error the second parameter is the receipt.
+        } catch (error) {
+            console.error(error)
+            process.exit(1)
         }
         console.log("AMPL was tipped")
+        process.exit()
     }
-    process.exit()
-
+    console.error('Not enough balance');
+    process.exit(1)
 }
+
+run()
