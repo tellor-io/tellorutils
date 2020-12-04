6767d6836a410bb9789f989db9611316dcb1da7b
brendaloya <l.loya.b@gmail.com>
2020-12-04 11:36:06 -0500

    update script 3

--- 03_verifyTellor.js
+++ 03_verifyTellor.js
@@ -110,7 +110,6 @@ let run = async function () {
 
             console.log("Verify Tellor's AMPL price")
             try{
-                //let tx = await amplIntertWithSigner.verifyTellorReports()
                 let tx = await amplIntertWithSigner.verifyTellorReports({ from: pubAddr, gasLimit: gas_limit, gasPrice: gasP })
                 var link = "".concat(etherscanUrl, '/tx/', tx.hash)
                 var ownerlink = "".concat(etherscanUrl, '/address/', pubAddr)
